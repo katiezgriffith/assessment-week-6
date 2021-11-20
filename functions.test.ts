6db1113bab153e9fd67b233c45feda5f5a343183
Katie Griffith <angel_9586@hotmail.com>
2021-11-20 16:13:59 -0600

    Added in 2 tests in functions.tes.ts

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,13 @@
 const {shuffleArray} = require('./utils')
 
-describe('shuffleArray should', () => {
-    // CODE HERE
+describe('shuffleArray should return an array', () => {
+    let result = shuffleArray('arrCopy')
+    test('Array Copy', () => expect(result).toEqual('arrCopy'))
+
 })
+
+describe('the array has j in it', () => {
+    let result = shuffleArray('arrCopy')
+    test("Contaains j", () => expect(result).toContain('j'))
+   
+});
